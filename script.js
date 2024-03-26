9673b406353d02e80018679f48b19733a4c6eb05
Sandesh Nepal <isandesh47@gmail.com>
2024-03-26 13:39:03 +0545

    Plant adde

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ window.onload = () => {
   startGame();
 };
 let currentMolePosition;
+let currentPlantPosition;
 function startGame() {
   for (let i = 0; i < 9; i++) {
     let newDiv = document.createElement("div");
     newDiv.id = i.toString();
     document.getElementById("board").appendChild(newDiv);
   }
-  setMole();
+  setInterval(() => {
+    setMole();
+  }, 1000);
+  setInterval(() => {
+    setPlant();
+  }, 2000);
 }
 
 function setMole() {
@@ -17,4 +23,12 @@ function setMole() {
   let randomNum = Math.floor(Math.random() * 9);
   currentMolePosition = document.getElementById(randomNum);
   currentMolePosition.appendChild(moleDiv);
+}
+
+function setPlant() {
+  let plantDiv = document.createElement("img");
+  plantDiv.src = "./piranha-plant.png";
+  let randomNum = Math.floor(Math.random() * 9);
+  currentPlantPosition = document.getElementById(randomNum);
+  currentMolePosition.appendChild(plantDiv);
 }
