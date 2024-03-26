3c323d901eee6f10a06d9a0798c1ac16d8072f9f
Sandesh Nepal <isandesh47@gmail.com>
2024-03-26 14:24:35 +0545

    Game Completed

--- script.js
+++ script.js
@@ -3,32 +3,63 @@ window.onload = () => {
 };
 let currentMolePosition;
 let currentPlantPosition;
+let gameOver = false;
+let userScore = 0;
 function startGame() {
   for (let i = 0; i < 9; i++) {
     let newDiv = document.createElement("div");
     newDiv.id = i.toString();
+    newDiv.addEventListener("click", selectTile);
     document.getElementById("board").appendChild(newDiv);
   }
-  setInterval(() => {
-    setMole();
-  }, 1000);
-  setInterval(() => {
-    setPlant();
-  }, 2000);
+  setInterval(setMole, 1000);
+  setInterval(setPlant, 2000);
 }
 
 function setMole() {
+  if (gameOver) {
+    return;
+  }
+  if (currentMolePosition) {
+    currentMolePosition.innerHTML = "";
+  }
   let moleDiv = document.createElement("img");
   moleDiv.src = "./monty-mole.png";
   let randomNum = Math.floor(Math.random() * 9);
+  if (currentPlantPosition && currentPlantPosition.id == randomNum) {
+    return;
+  }
   currentMolePosition = document.getElementById(randomNum);
   currentMolePosition.appendChild(moleDiv);
 }
 
 function setPlant() {
+  if (gameOver) {
+    return;
+  }
+  if (currentPlantPosition) {
+    currentPlantPosition.innerHTML = "";
+  }
   let plantDiv = document.createElement("img");
   plantDiv.src = "./piranha-plant.png";
   let randomNum = Math.floor(Math.random() * 9);
+  if (currentMolePosition && currentMolePosition.id == randomNum) {
+    return;
+  }
   currentPlantPosition = document.getElementById(randomNum);
-  currentMolePosition.appendChild(plantDiv);
+  currentPlantPosition.appendChild(plantDiv);
+}
+
+function selectTile() {
+  if (this == currentMolePosition) {
+    userScore += 10;
+    document.getElementById("score").innerText =
+      "Current Score: " + userScore.toString();
+    console.log("Clicked");
+  }
+  if (this == currentPlantPosition) {
+    document.getElementById("score").innerText =
+      "Game Over Your Score: " + userScore;
+    gameOver = true;
+  }
 }
